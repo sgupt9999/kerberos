4a0b118da6807f748c0070824176f4b294af4c96
root <root@garfield99994.mylabserver.com>
2018-05-02 03:05:28 +0000

    kerberos-server. Looks to be working. Still more testing needed.

--- kerberos-server.sh
+++ kerberos-server.sh
@@ -6,33 +6,24 @@
 REALM="MYSERVER.COM"
 DOMAIN="mylabserver.com"
 IPKDC=172.31.111.114
-IPSERVER=0.0.0.0
+IPSERVER=172.31.124.129
 IPCLIENT=172.31.125.24
 HOSTS="/etc/hosts"
 HOSTKDC="garfield99996.mylabserver.com"
-HOSTSERVER="garfield99995.mylabserver.com"
-HOSTCLIENT="garfield99994.mylabserver.com"
-KDADMFILE="/var/kerberos/krb5kdc/kadm5.acl"
-KDCCONFFILE="/var/kerberos/krb5kdc/kdc.conf"
+HOSTSERVER="garfield99994.mylabserver.com"
+HOSTCLIENT="garfield99995.mylabserver.com"
 KRBCONFFILE="/etc/krb5.conf"
 KRBCONFBKFILE="/etc/krb5_backup.conf"
 
 
-KDCDBPASSWORD="redhat"
-KDCROOTPASSWD="redhat"
+KDCROOTPASSWORD="redhat"
 USER1="krbtest"
 
 # The host service is used for login or ssh
 SERVICE1="host/$HOSTSERVER"
-
-#FIREWALL="yes"
-FIREWALL="no"
 # End of user inputs
 
-
 SERVERPACKAGES="krb5-workstation"
-CLIENTPACKAGES="krb5-workstation pam_krb5"
-
 
 if [[ $EUID != "0" ]]
 then
@@ -57,25 +48,26 @@ echo "Done"
 # exist
 if [ -f $KRBCONFBKFILE ]
 then
-	echo "Backup file exists"
+	echo "copying backup file"
 	cp -f $KRBCONFBKFILE $KRBCONFFILE
 else
-	echo "Creating backup file"
 	cp -f $KRBCONFFILE $KRBCONFBKFILE
 fi
 
 echo "Updating krb5.conf"
 sed -i "s/#.*default_realm.*/default_realm = $REALM/" $KRBCONFFILE
-sed -i "s/#.*EXAMPLE.COM.*/$REALM = {/" $KRBCONFFILE
 sed -i "s/#.*kdc.*/kdc = $HOSTKDC/" $KRBCONFFILE
 sed -i "s/#.*admin_server.*/admin_server = $HOSTKDC/" $KRBCONFFILE
 sed -i "s/#.*}/}/" $KRBCONFFILE
-sed -i "s/#.*.example.com.*/.$DOMAIN = $REALM/" $KRBCONFFILE
-sed -i "s/#.*example.com.*/$DOMAIN = $REALM/" $KRBCONFFILE
+sed -i "s/#.*.example.com.*=.*EXAMPLE.COM.*/.$DOMAIN = $REALM/" $KRBCONFFILE
+sed -i "s/#.*example.com.*=.*EXAMPLE.COM.*/$DOMAIN = $REALM/" $KRBCONFFILE
+# Not sure why but the EXAMPLE.COM is changing example.com as well. This section of code can be
+# cleaned up
+sed -i "s/#.*EXAMPLE.COM.*/$REALM = {/" $KRBCONFFILE
 
 # Add the host service to the KDC database and save the key to a local keytab file
-kadmin -w $KDCDBPASSWORD -q "addprinc -randkey $SERVICE"
-kadmin -w $KDCDBPASSWORD -q "ktadd $SERVICE"
+kadmin -p root/admin -w $KDCROOTPASSWORD -q "addprinc -randkey $SERVICE1"
+kadmin -p root/admin -w $KDCROOTPASSWORD -q "ktadd $SERVICE1"
 
 # Add user for testing
 useradd $USER1
